0c239a0e6c34e1cd1876b1eadd568384ff046344
Sergey Butenko <sereja.butenko@gmail.com>
2014-10-25 09:08:19 +0300

    Initial commit

---
+++ EvaqAlarm/EAMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  EAMainViewController.h
+//  EvaqAlarm
+//
+//  Created by Sergey Butenko on 10/25/14.
+//  Copyright (c) 2014 Speaking Mind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAMainViewController : UIViewController
+
+@end
---
+++ EvaqAlarm/EAMainViewController.m
@@ -0,0 +1,13 @@
+//
+//  EAMainViewController.m
+//  EvaqAlarm
+//
+//  Created by Sergey Butenko on 10/25/14.
+//  Copyright (c) 2014 Speaking Mind. All rights reserved.
+//
+
+#import "EAMainViewController.h"
+
+@implementation EAMainViewController
+
+@end
